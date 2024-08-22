cc4dc05a9ba64786f24ddfbad50224f173910fe1
Md Abdul Mottalib <abdulmottalib2004@gmail.com>
2024-08-22 18:55:22 +0600

    files path fixing

--- public/pages/update_expense_ajax.php
+++ public/pages/update_expense_ajax.php
@@ -1,16 +1,22 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Expense.php';
 
-$expenseModel = new Expense();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $expenseModel = new Expense();
 
-$expenseId = $_POST['id'];
-$budget_amount = $_POST['budget_amount'];
-$actual_amount = $_POST['actual_amount'];
-$date = $_POST['date'];
-$description = $_POST['description'];
+    $expenseId = $_POST['id'];
+    $budget_amount = $_POST['budget_amount'];
+    $actual_amount = $_POST['actual_amount'];
+    $date = $_POST['date'];
+    $description = $_POST['description'];
 
-$success = $expenseModel->updateExpense($expenseId, null, $budget_amount, $actual_amount, $date, $description);
+    // Assuming updateExpense method returns true on success and false on failure
+    $success = $expenseModel->updateExpense($expenseId, null, $budget_amount, $actual_amount, $date, $description);
 
-header('Content-Type: application/json');
-echo json_encode(['success' => $success]);
+    header('Content-Type: application/json');
+    echo json_encode(['success' => $success]);
+} else {
+    header('Content-Type: application/json');
+    echo json_encode(['success' => false, 'message' => 'Invalid request method']);
+}
 ?>
